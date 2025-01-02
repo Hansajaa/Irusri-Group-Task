0f3217f91d25760bfbb3f915c8b044ab1621323c
Hansajaa <nilanahansaja21@gmail.com>
2025-01-02 12:01:36 +0530

    Bug Fix in Utils

--- src/api/Utils.jsx
+++ src/api/Utils.jsx
@@ -1,18 +1,32 @@
 import axios from "axios";
-import process from "process";
 
 const apiClient = axios.create({
     baseURL: "https://www.googleapis.com/books/v1",
     timeout: 10000,
+    headers: {
+        'Content-Type': 'application/json'
+    }
 });
 
-export const apiRequest = async (method, url, data = null) => {
+export const apiRequest = async (method, url, data = null, params = {}) => {
+
     try {
+
+        // Assign API key from Vite env
+        const apiKey = import.meta.env.VITE_GOOGLE_BOOKS_API_KEY;
+
+        const requestParams = {
+            ...params,
+            key: apiKey,
+        }
+
         const response = await apiClient({
             method,
-            url: `${url}&Key=${process.env.BOOKSTORE_GOOGLE_BOOK_API_KEY}`,
+            url,
             data,
+            params: requestParams,
         });
+
         return response.data;
     } catch (error) {
         console.error("API request error:", error);
